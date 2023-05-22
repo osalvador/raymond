7aa094a0c5f56128424b638a1c6a13f23070bedc
Oscar Salvador Magallanes <osalvador@gmail.com>
2023-05-22 11:39:44 +0200

    fix: update dependencies

--- parser/parser.go
+++ parser/parser.go
@@ -7,8 +7,8 @@ import (
 	"runtime"
 	"strconv"
 
-	"github.com/aymerick/raymond/ast"
-	"github.com/aymerick/raymond/lexer"
+	"github.com/osalvador/raymond/ast"
+	"github.com/osalvador/raymond/lexer"
 )
 
 // References:
